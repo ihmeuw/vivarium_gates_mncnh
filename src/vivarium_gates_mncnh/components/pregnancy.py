c33022af4938517ccb46745e68e9425f5985cbe8
albrja <albrja@uw.edu>
2024-08-28 16:41:10 -0700

    Intermediary commit

--- src/vivarium_gates_mncnh/components/pregnancy.py
+++ src/vivarium_gates_mncnh/components/pregnancy.py
@@ -5,6 +5,7 @@ from vivarium.framework.population import SimulantData
 
 from vivarium_gates_mncnh.constants import data_keys
 from vivarium_gates_mncnh.constants.metadata import ARTIFACT_INDEX_COLUMNS
+from vivarium_gates_mncnh.constants.data_values import PREGNANCY_OUTCOMES
 
 
 class Pregnancy(Component):
@@ -50,7 +51,7 @@ class Pregnancy(Component):
 
     def on_initialize_simulants(self, pop_data: SimulantData) -> None:
         # TODO: Assign partial or full term duration according to table in `Pregnancy term lengths`_ section
-        pop_update = pop_data["pregnancy_duration"] = self.determine_pregnancy_duration(
+        pregnancy_outcomes_and_durations = self.sample_pregnancy_outcomes_and_durations(
             pop_data
         )
         # TODO: Assign sex of infant if pregnancy is full term (stillbirth or live birth)
@@ -59,11 +60,69 @@ class Pregnancy(Component):
         self.population_view.update(pop_update)
 
     def get_birth_outcome_probabilities(self, builder: Builder) -> pd.DataFrame:
-        # TODO: get birth outcome probabilities
-        return pd.DataFrame()
+        asfr = builder.data.load(data_keys.PREGNANCY.ASFR).set_index(ARTIFACT_INDEX_COLUMNS)
+        sbr = (
+            builder.data.load(data_keys.PREGNANCY.SBR)
+            .set_index("year_start")
+            .drop(columns=["year_end"])
+            .reindex(asfr.index, level="year_start")
+        )
 
-    def determine_pregnancy_duration(self, pop_data: SimulantData) -> pd.DataFrame:
-        duration_df = pop_data.copy()
-        # TODO: get pregnancy duration
+        raw_incidence_miscarriage = builder.data.load(
+            data_keys.PREGNANCY.RAW_INCIDENCE_RATE_MISCARRIAGE
+        ).set_index(ARTIFACT_INDEX_COLUMNS)
+        raw_incidence_ectopic = builder.data.load(
+            data_keys.PREGNANCY.RAW_INCIDENCE_RATE_ECTOPIC
+        ).set_index(ARTIFACT_INDEX_COLUMNS)
 
-        return duration_df
+        total_incidence = (
+            asfr
+            + asfr.multiply(sbr["value"], axis=0)
+            + raw_incidence_ectopic
+            + raw_incidence_miscarriage
+        )
+
+        partial_term = (raw_incidence_ectopic + raw_incidence_miscarriage) / total_incidence
+        partial_term["pregnancy_outcome"] = PREGNANCY_OUTCOMES.PARTIAL_TERM_OUTCOME
+        live_births = asfr / total_incidence
+        live_births["pregnancy_outcome"] = PREGNANCY_OUTCOMES.LIVE_BIRTH_OUTCOME
+        stillbirths = asfr.multiply(sbr["value"], axis=0) / total_incidence
+        stillbirths["pregnancy_outcome"] = PREGNANCY_OUTCOMES.STILLBIRTH_OUTCOME
+        probabilities = pd.concat([partial_term, live_births, stillbirths])
+        probabilities = probabilities.pivot(
+            columns="pregnancy_outcome", values="value"
+        ).reset_index()
+        return probabilities
+
+    def sample_pregnancy_outcomes_and_durations(self, pop_data: SimulantData) -> pd.DataFrame:
+        # Order the columns so that partial_term isn't in the middle!
+        outcome_probabilities = self.birth_outcome_probabilities(pop_data.index)[
+            ["partial_term", "stillbirth", "live_birth"]
+        ]
+        pregnancy_outcomes = pd.DataFrame(
+            {
+                "pregnancy_outcome": self.randomness.choice(
+                    pop_data.index,
+                    choices=outcome_probabilities.columns.to_list(),
+                    p=outcome_probabilities,
+                    additional_key="pregnancy_outcome",
+                )
+            }
+        )
+
+        term_child_map = {
+            PREGNANCY_OUTCOMES.STILLBIRTH_OUTCOME: self.sample_full_term_durations,
+            PREGNANCY_OUTCOMES.LIVE_BIRTH_OUTCOME: self.sample_full_term_durations,
+            PREGNANCY_OUTCOMES.PARTIAL_TERM_OUTCOME: self.sample_partial_term_durations,
+        }
+
+        for term_length, sampling_function in term_child_map.items():
+            term_pop = pregnancy_outcomes[
+                pregnancy_outcomes["pregnancy_outcome"] == term_length
+            ].index
+            pregnancy_outcomes.loc[
+                term_pop,
+                ["sex_of_child", "birth_weight", "gestational_age", "pregnancy_duration"],
+            ] = sampling_function(term_pop)
+
+        return pregnancy_outcomes
