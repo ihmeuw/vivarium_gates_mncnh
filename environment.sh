e741cebc334218e5402031330ed7dcc74a69a8bb
albrja <albrja@uw.edu>
2024-09-04 11:54:27 -0700

    Update creation time

--- environment.sh
+++ environment.sh
@@ -68,7 +68,7 @@ else
   # Check if existing environment needs to be recreated
   echo "Existing environment found for $env_name."
   one_week_ago=$(date -d "7 days ago" '+%Y-%m-%d %H:%M:%S')
-  creation_time="$(head -n1 /home/$username/miniconda3/envs/$env_name/conda-meta/history)"
+  creation_time="$(head -n1 $CONDA_PREFIX/conda-meta/history)"
   requirements_modification_time="$(date -r $install_file '+%Y-%m-%d %H:%M:%S')"
   # Check if existing environment is older than a week or if environment was built 
   # before last modification to requirements file. If so, mark for recreation.
