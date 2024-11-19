0403a6479e3b39062c7d8e3b31607662ada5fa03
Jim Albright <37345113+albrja@users.noreply.github.com>
2024-11-19 14:15:44 -0800

    Albrja/mic 5578/maternal sepsis artifact (#9)
    Albrja/mic 5578/maternal sepsis artifact
    
    Maternal Sepsis
    - *Category*: Artifact
    - *JIRA issue*: https://jira.ihme.washington.edu/browse/MIC-5578
    - *Research reference*: https://vivarium-research.readthedocs.io/en/latest/models/causes/maternal_disorders/gbd_2021_mncnh/maternal_sepsis.html#id6
    
    Changes and notes
    -add maternal sepsis data to artifact
    
    Verification and Testing
    Successfully ran Ethiopia artifact

--- src/vivarium_gates_mncnh/data/loader.py
+++ src/vivarium_gates_mncnh/data/loader.py
@@ -64,6 +64,8 @@ def get_data(
         data_keys.LBWSG.CATEGORIES: load_metadata,
         data_keys.LBWSG.EXPOSURE: load_lbwsg_exposure,
         data_keys.ANC.ESTIMATE: load_anc_proportion,
+        data_keys.MATERNAL_SEPSIS.RAW_INCIDENCE_RATE: load_standard_data,
+        data_keys.MATERNAL_SEPSIS.CSMR: load_standard_data,
     }
     return mapping[lookup_key](lookup_key, location, years)
 
