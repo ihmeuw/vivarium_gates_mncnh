3864f69efd8a3ae5bb88c8d68b1d13fe921d327d
Jim Albright <37345113+albrja@users.noreply.github.com>
2024-12-11 09:31:54 -0800

    Albrja/mic 5610/maternal disorders observer (#18)
    Albrja/mic 5610/maternal disorders observer
    
    Observer maternal disorders burden
    - *Category*: Observers
    - *JIRA issue*: https://jira.ihme.washington.edu/browse/MIC-5610
    - *Research reference*: https://vivarium-research.readthedocs.io/en/latest/models/causes/maternal_disorders/gbd_2021_mncnh/maternal_hemorrhage.html#validation-criteria
    
    Changes and notes
    -add observer for maternal disorders
    
    Verification and Testing
    Ran simulation with expected recorded results being saved to outputs.

--- src/vivarium_gates_mncnh/components/observers.py
+++ src/vivarium_gates_mncnh/components/observers.py
@@ -1,12 +1,20 @@
+from functools import partial
 from typing import Any
 
 import pandas as pd
 from vivarium.framework.engine import Builder
+from vivarium.framework.event import Event
 from vivarium.framework.results import Observer
-from vivarium_public_health.results import COLUMNS, PublicHealthObserver
+from vivarium_public_health.results import COLUMNS
 from vivarium_public_health.results import ResultsStratifier as ResultsStratifier_
 
-from vivarium_gates_mncnh.constants.data_values import COLUMNS, PREGNANCY_OUTCOMES
+from vivarium_gates_mncnh.constants.data_values import (
+    COLUMNS,
+    MATERNAL_DISORDERS,
+    PREGNANCY_OUTCOMES,
+    SIMULATION_EVENT_NAMES,
+)
+from vivarium_gates_mncnh.constants.metadata import ARTIFACT_INDEX_COLUMNS
 
 
 class ResultsStratifier(ResultsStratifier_):
@@ -80,3 +88,102 @@ class ANCObserver(Observer):
                 COLUMNS.PREGNANCY_OUTCOME,
             ],
         )
+
+
+class MaternalDisordersBurdenObserver(Observer):
+    @property
+    def configuration_defaults(self) -> dict[str, Any]:
+        return {
+            "stratification": {
+                self.get_configuration_name(): {
+                    "exclude": [],
+                    "include": [],
+                    "data_sources": {
+                        f"{cause}_ylds": partial(self.load_ylds_per_case, cause=cause)
+                        for cause in self.maternal_disorders
+                    },
+                },
+            },
+        }
+
+    def __init__(self):
+        super().__init__()
+        self.maternal_disorders = MATERNAL_DISORDERS
+
+    def setup(self, builder: Builder) -> None:
+        self._sim_step_name = builder.time.simulation_event_name()
+
+    def get_configuration(self, builder):
+        return builder.configuration["stratification"][self.get_configuration_name()]
+
+    def register_observations(self, builder: Builder) -> None:
+        dead_pop_filter = f"{COLUMNS.ALIVE} == 'dead'"
+        builder.results.register_stratification(
+            "cause_of_maternal_death",
+            MATERNAL_DISORDERS + ["not_dead"],
+            excluded_categories=["not_dead"],
+            requires_columns=[COLUMNS.CAUSE_OF_DEATH],
+        )
+
+        builder.results.register_adding_observation(
+            name="maternal_disorder_deaths",
+            pop_filter=dead_pop_filter,
+            requires_columns=[COLUMNS.ALIVE],
+            additional_stratifications=self.configuration.include
+            + ["cause_of_maternal_death"],
+            excluded_stratifications=self.configuration.exclude,
+            to_observe=self.to_observe,
+        )
+        builder.results.register_adding_observation(
+            name="maternal_disorder_ylls",
+            pop_filter=dead_pop_filter,
+            requires_columns=[COLUMNS.ALIVE, COLUMNS.YEARS_OF_LIFE_LOST],
+            additional_stratifications=self.configuration.include
+            + ["cause_of_maternal_death"],
+            excluded_stratifications=self.configuration.exclude,
+            to_observe=self.to_observe,
+            aggregator=self.calculate_ylls,
+        )
+        for cause in self.maternal_disorders:
+            builder.results.register_adding_observation(
+                name=f"{cause}_counts",
+                pop_filter=f"{cause} == True",
+                requires_columns=[cause],
+                additional_stratifications=self.configuration.include,
+                excluded_stratifications=self.configuration.exclude,
+                to_observe=self.to_observe,
+            )
+            builder.results.register_adding_observation(
+                name=f"{cause}_ylds",
+                pop_filter=f"{cause} == True",
+                requires_columns=[cause],
+                additional_stratifications=self.configuration.include,
+                excluded_stratifications=self.configuration.exclude,
+                to_observe=self.to_observe,
+                aggregator=partial(self.calculate_ylds, cause=cause),
+            )
+
+    def to_observe(self, event: Event) -> bool:
+        return self._sim_step_name() == SIMULATION_EVENT_NAMES.MORTALITY
+
+    def calculate_ylls(self, data: pd.DataFrame) -> float:
+        return data[COLUMNS.YEARS_OF_LIFE_LOST].sum()
+
+    def calculate_ylds(self, data: pd.DataFrame, cause: str) -> float:
+        yld_per_case = self.lookup_tables[f"{cause}_ylds"](data.index)
+        return yld_per_case.sum()
+
+    ##################
+    # Helper methods #
+    ##################
+
+    def load_ylds_per_case(self, builder: Builder, cause: str) -> pd.DataFrame:
+        yld_rate = builder.data.load(f"cause.{cause}.yld_rate").set_index(
+            ARTIFACT_INDEX_COLUMNS
+        )
+        incidence_rate = builder.data.load(f"cause.{cause}.incidence_rate").set_index(
+            ARTIFACT_INDEX_COLUMNS
+        )
+        ylds = (yld_rate / incidence_rate).fillna(0).reset_index()
+
+        return ylds
