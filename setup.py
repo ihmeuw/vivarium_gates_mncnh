2442776919426519f7d149ec0c149a94df95ccc1
Jim Albright <albrja@uw.edu>
2025-02-25 15:24:13 -0800

    Update setup

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
     data_requirements = ["vivarium_inputs[data]>=5.0.5"]
     cluster_requirements = ["vivarium_cluster_tools>=2.0.0"]
-    test_requirements = ["pytest", "pytest-cov"]
+    test_requirements = ["pytest", "pytest-cov", "vivarium_testing_utils"]
     lint_requirements = ["black==22.3.0", "isort==5.13.2"]
 
     setup(
