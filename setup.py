2a3e5f802b3012aacf759fa8cee3fc600fcd3980
Steve Bachmeier <sbachmei@uw.edu>
2025-07-30 11:42:43 -0700

    clean up lint requirements

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
         "vivarium_testing_utils",
     ]
     lint_requirements = [
-        "vivarium_dependencies[format]",
+        "vivarium_dependencies[lint]",
     ]
     interactive_requirements = [
         "vivarium_dependencies[interactive]",
