eed25ca5d7b66d0b665863c209cfc5159f7bc82e
Hussain Jafari <hussainsjafari@gmail.com>
2025-10-17 14:42:35 -0700

    GBD 2023 non-RT artifact updates (#162)
    Category: artifact
    JIRA issue: https://jira.ihme.washington.edu/browse/MIC-6474
    
    Changes and notes
    Update to GBD 2023.
    
    Verification and Testing
    Checked that year data in artifact was 2023 and that all relevant data had 250 draws.
    Ran simulation to completion with new artifact with RT-owned keys filled in with model 18.1 artifact data.

--- src/vivarium_gates_mncnh/data/extra_gbd.py
+++ src/vivarium_gates_mncnh/data/extra_gbd.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from vivarium_gbd_access import constants as gbd_constants
 from vivarium_gbd_access import utilities as vi_utils
+from vivarium_gbd_access.gbd import base_data
 from vivarium_inputs import globals as vi_globals
 from vivarium_inputs import utility_data
 
@@ -16,13 +17,13 @@ from vivarium_gates_mncnh.data import utilities
 def get_maternal_disorder_yld_rate(key: str, location: str) -> pd.DataFrame:
     entity = utilities.get_entity(key)
     location_id = utility_data.get_location_id(location)
-    data = vi_utils.get_draws(
+    data = base_data.get_draws(
         "cause_id",
         entity.gbd_id,
         source=gbd_constants.SOURCES.COMO,
         location_id=location_id,
         year_id=ARTIFACT_YEAR_START,
-        release_id=gbd_constants.RELEASE_IDS.GBD_2021,
+        release_id=gbd_constants.RELEASE_IDS.GBD_2023,
         measure_id=vi_globals.MEASURES["YLDs"],
         metric_id=vi_globals.METRICS["Rate"],
     )
@@ -30,19 +31,37 @@ def get_maternal_disorder_yld_rate(key: str, location: str) -> pd.DataFrame:
 
 
 @vi_utils.cache
-def load_lbwsg_exposure(location: str) -> pd.DataFrame:
+def load_2021_lbwsg_birth_exposure(location: str) -> pd.DataFrame:
     entity = utilities.get_entity(data_keys.LBWSG.BIRTH_EXPOSURE)
     location_id = utility_data.get_location_id(location)
-    data = vi_utils.get_draws(
+    data = base_data.get_draws(
         gbd_id_type="rei_id",
         gbd_id=entity.gbd_id,
         source=gbd_constants.SOURCES.EXPOSURE,
         location_id=location_id,
-        year_id=ARTIFACT_YEAR_START,
+        year_id=2022,
         sex_id=gbd_constants.SEX.MALE + gbd_constants.SEX.FEMALE,
         age_group_id=164,  # Birth prevalence
         release_id=gbd_constants.RELEASE_IDS.GBD_2021,
     )
+    data["year_id"] = ARTIFACT_YEAR_START
+    return data
+
+
+@vi_utils.cache
+def load_2021_lbwsg_rr(location: str) -> pd.DataFrame:
+    entity = utilities.get_entity(data_keys.LBWSG.RELATIVE_RISK)
+    location_id = utility_data.get_location_id(location)
+    data = base_data.get_draws(
+        gbd_id_type="rei_id",
+        gbd_id=entity.gbd_id,
+        source=gbd_constants.SOURCES.RR,
+        location_id=location_id,
+        year_id=2022,
+        sex_id=gbd_constants.SEX.MALE + gbd_constants.SEX.FEMALE,
+        release_id=gbd_constants.RELEASE_IDS.GBD_2021,
+    )
+    data["year_id"] = ARTIFACT_YEAR_START
     return data
 
 
@@ -52,7 +71,7 @@ def get_birth_counts(location: str) -> pd.DataFrame:
 
     location_id = utility_data.get_location_id(location)
     births = get_population(
-        release_id=gbd_constants.RELEASE_IDS.GBD_2021,
+        release_id=gbd_constants.RELEASE_IDS.GBD_2023,
         location_id=location_id,
         age_group_id=GBD_BIRTH_AGE_GROUP_ID,
         year_id=ARTIFACT_YEAR_START,
@@ -68,8 +87,8 @@ def get_birth_counts(location: str) -> pd.DataFrame:
 @vi_utils.cache
 def get_mortality_death_counts(location: str, age_group_id: int, gbd_id: int) -> pd.DataFrame:
     location_id = utility_data.get_location_id(location)
-    data = vi_utils.get_draws(
-        release_id=gbd_constants.RELEASE_IDS.GBD_2021,
+    data = base_data.get_draws(
+        release_id=gbd_constants.RELEASE_IDS.GBD_2023,
         location_id=location_id,
         age_group_id=age_group_id,
         gbd_id_type="cause_id",
@@ -92,7 +111,7 @@ def get_hemoglobin_exposure_data(key: str, location: str) -> pd.DataFrame:
     }
     source = source_map[key]
     location_id = utility_data.get_location_id(location)
-    data = vi_utils.get_draws(
+    data = base_data.get_draws(
         gbd_id_type="rei_id",
         gbd_id=376,
         source=source,
@@ -106,7 +125,7 @@ def get_hemoglobin_exposure_data(key: str, location: str) -> pd.DataFrame:
 
 @vi_utils.cache
 def get_hemoglobin_rr_data(key: str, location: str) -> pd.DataFrame:
-    data = vi_utils.get_draws(
+    data = base_data.get_draws(
         release_id=33,
         gbd_id_type="rei_id",
         gbd_id=376,
@@ -115,13 +134,14 @@ def get_hemoglobin_rr_data(key: str, location: str) -> pd.DataFrame:
         location_id=1,  # global data
         year_id=2022,
     )
+    data["year_id"] = 2023
     return data
 
 
 @vi_utils.cache
 def get_hemoglobin_paf_data(key: str, location: str) -> pd.DataFrame:
     location_id = utility_data.get_location_id(location)
-    data = vi_utils.get_draws(
+    data = base_data.get_draws(
         release_id=33,
         version_id=393,
         gbd_id_type="rei_id",
