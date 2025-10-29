ihmeuw/vivarium_gates_mncnh

commit 1fe36468dcb76358703be7bc04b63302e69c41ea

model 18.2 bugfixes (#154)
Category: bugfix
JIRA issue: https://jira.ihme.washington.edu/browse/MIC-6464
Research reference: oral iron effects

Changes and notes
Use final hemoglobin exposure when calculating risk effects by updating hemoglobin exposure population column.
Use cat2 coverage instead of cat1 to determine IFA coverage.
Use ANC-scaled IFA coverage when calculating IFA-deleted hemoglobin and calculating IFA on birthweight risk specific shift.
Remove birth weight effects from IFA effect size key.
Use seed when generating correlated propensities.

Verification and Testing
Checked in interactive context that hemoglobin exposure in state table matches final hemoglobin exposure pipeline value.

*** REMINDER ***
CI WILL NOT RUN ANY TESTS.
MANUALLY RUN TESTS WITH EACH PR.
-->

 all tests pass (pytest --runslow)

src/vivarium_gates_mncnh/data/loader.py
@@ -1065,17 +1065,12 @@ def load_coverage_from_file(filepath: Path, location: str) -> pd.DataFrame:
 def load_oral_iron_effect_size(
     key: str, location: str, years: Optional[Union[int, str, List[int]]] = None
 ) -> pd.DataFrame:
-    effect_size_dists = data_values.ORAL_IRON_EFFECT_SIZES[key]
-    effect_size_data = []
-
-    for target, dist in effect_size_dists.items():
-        draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
-        data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
-        data["affected_target"] = target
-        data = data.set_index("affected_target")
-        effect_size_data.append(data)
-
-    return pd.concat(effect_size_data)
+    dist = data_values.ORAL_IRON_EFFECT_SIZES[key]["hemoglobin.exposure"]
+    draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
+    data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
+    data["affected_target"] = "hemoglobin.exposure"
+    data = data.set_index("affected_target")
+    return data
 
 
 def load_ifa_excess_shift(
@@ -1107,9 +1102,10 @@ def load_risk_specific_shift(
     else:
         exposure = get_data(key_group.COVERAGE, location)
         excess_shift = get_data(key_group.EXCESS_SHIFT, location)
+        anc_proportion = get_data(data_keys.ANC.ANC1, location)
 
         risk_specific_shift = (
-            (exposure * excess_shift)
+            (exposure * excess_shift * anc_proportion)
             .groupby(
                 metadata.ARTIFACT_INDEX_COLUMNS + ["affected_entity", "affected_measure"]
             )

src/vivarium_gates_mncnh/components/hemoglobin.py
@@ -4,22 +4,35 @@ import numpy as np
 import pandas as pd
 import scipy
 from vivarium.framework.engine import Builder
+from vivarium.framework.event import Event
 from vivarium_public_health.risks.base_risk import Risk
 from vivarium_public_health.risks.distributions import MissingDataError
 from vivarium_public_health.risks.effect import NonLogLinearRiskEffect
 
 from vivarium_gates_mncnh.constants import data_keys
-from vivarium_gates_mncnh.constants.data_values import PIPELINES
+from vivarium_gates_mncnh.constants.data_values import PIPELINES, SIMULATION_EVENT_NAMES
 
 
 class Hemoglobin(Risk):
+    @property
+    def time_step_cleanup_priority(self) -> int:
+        # oral iron intervention will have been assigned
+        # when we update hemoglobin exposure in the state table
+        return 7
+
     def __init__(self) -> None:
         super().__init__("risk_factor.hemoglobin")
         self.exposure_pipeline_name = f"raw_{self.risk.name}.exposure"
 
     def setup(self, builder: Builder) -> None:
         super().setup(builder)
-        self.ifa_coverage = builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE).value[0]
+        self._sim_step_name = builder.time.simulation_event_name()
+        self.ifa_coverage = (
+            builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE)
+            .query("parameter=='cat2'")
+            .reset_index()
+            .value[0]
+        )
         self.ifa_effect_size = builder.data.load(
             data_keys.IFA_SUPPLEMENTATION.EFFECT_SIZE
         ).value[0]
@@ -29,14 +42,30 @@ class Hemoglobin(Risk):
             self.get_ifa_deleted_hemoglobin,
             component=self,
         )
-        builder.value.register_value_producer(
+        self.final_hemoglobin_exposure = builder.value.register_value_producer(
             PIPELINES.HEMOGLOBIN_EXPOSURE,
             source=lambda index: self.ifa_deleted_hemoglobin(index),
             component=self,
         )
 
+    def build_all_lookup_tables(self, builder: Builder) -> None:
+        self.lookup_tables["ANC1"] = self.build_lookup_table(
+            builder=builder,
+            data_source=builder.data.load(data_keys.ANC.ANC1),
+            value_columns=["value"],
+        )
+
     def get_ifa_deleted_hemoglobin(self, index: pd.Index) -> pd.Series[float]:
-        return self.exposure(index) - (self.ifa_effect_size * self.ifa_coverage)
+        return self.exposure(index) - (
+            self.ifa_effect_size * self.ifa_coverage * self.lookup_tables["ANC1"](index)
+        )
+
+    def on_time_step_cleanup(self, event: Event):
+        if self._sim_step_name() != SIMULATION_EVENT_NAMES.PREGNANCY:
+            return
+        exposure = self.final_hemoglobin_exposure(event.index)
+        exposure.name = self.exposure_column_name
+        self.population_view.update(exposure)
 
 
 class HemoglobinRiskEffect(NonLogLinearRiskEffect):

src/vivarium_gates_mncnh/components/intervention.py
@@ -222,7 +222,12 @@ class OralIronInterventionExposure(Component):
         self.randomness = builder.randomness.get_stream(self.name)
 
         self.scenario = builder.configuration.intervention.scenario
-        self.ifa_coverage = builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE).value[0]
+        self.ifa_coverage = (
+            builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE)
+            .query("parameter=='cat2'")
+            .reset_index()
+            .value[0]
+        )
 
         self.ifa_exposure_pipeline = builder.value.register_value_producer(
             self.ifa_exposure_pipeline_name,
@@ -303,7 +308,6 @@ class OralIronInterventionEffect(Component):
     #################
 
     def setup(self, builder: Builder) -> None:
-        self.ifa_coverage = builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE).value[0]
         self.mms_stillbirth_rr = builder.data.load(
             data_keys.MMN_SUPPLEMENTATION.STILLBIRTH_RR
         ).value[0]

src/vivarium_gates_mncnh/components/propensity.py
@@ -6,6 +6,7 @@ import pandas as pd
 from statsmodels.distributions.copula.api import GaussianCopula
 from vivarium import Component
 from vivarium.framework.engine import Builder
+from vivarium.framework.randomness.stream import get_hash
 
 from vivarium_gates_mncnh.constants import data_values
 from vivarium_gates_mncnh.constants.data_keys import PROPENSITY_CORRELATIONS
@@ -52,7 +53,9 @@ class CorrelatedPropensities(Component):
             correlation_matrix[i, j] = correlation_matrix[j, i] = correlation
 
         copula = GaussianCopula(correlation_matrix)
-        propensities = copula.rvs(self.pop_size)
+        propensities = copula.rvs(
+            self.pop_size, random_state=get_hash("correlated_propensities")
+        )
         propensities_df = pd.DataFrame(propensities, columns=self.component_names)
         return propensities_df
 

commit 15d6a6cbe9fb43006077e01d9ec09a4f9f812699

Fix bug with oral iron effect sizes function
This did not work for MMS, building an artifact from
scratch, because it hardcoded the target key.

This bug was introduced by https://github.com/ihmeuw/vivarium_gates_mncnh/commit/1fe36468dcb76358703be7bc04b63302e69c41ea#diff-c5d0e2dfb27ec86d7fb3005cf7080be104354f2bc93bca0724a0c510e4b0b304

I must admit, I do not really understand the purpose of that change.
Possibly a Chesterton's Fence blunder.

src/vivarium_gates_mncnh/data/loader.py
@@ -1108,12 +1108,17 @@ def load_coverage_from_file(filepath: Path, location: str) -> pd.DataFrame:
 def load_oral_iron_effect_size(
     key: str, location: str, years: Optional[Union[int, str, List[int]]] = None
 ) -> pd.DataFrame:
-    dist = data_values.ORAL_IRON_EFFECT_SIZES[key]["hemoglobin.exposure"]
-    draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
-    data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
-    data["affected_target"] = "hemoglobin.exposure"
-    data = data.set_index("affected_target")
-    return data
+    effect_size_dists = data_values.ORAL_IRON_EFFECT_SIZES[key]
+    effect_size_data = []
+
+    for target, dist in effect_size_dists.items():
+        draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
+        data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
+        data["affected_target"] = target
+        data = data.set_index("affected_target")
+        effect_size_data.append(data)
+
+    return pd.concat(effect_size_data)
 
 
 def load_ifa_excess_shift(