ihmeuw/vivarium_gates_mncnh

commit 1fe36468dcb76358703be7bc04b63302e69c41ea

model 18.2 bugfixes (#154)
Category: bugfix
JIRA issue: https://jira.ihme.washington.edu/browse/MIC-6464
Research reference: oral iron effects

Changes and notes
Use final hemoglobin exposure when calculating risk effects by updating hemoglobin exposure population column.
Use cat2 coverage instead of cat1 to determine IFA coverage.
Use ANC-scaled IFA coverage when calculating IFA-deleted hemoglobin and calculating IFA on birthweight risk specific shift.
Remove birth weight effects from IFA effect size key.
Use seed when generating correlated propensities.

Verification and Testing
Checked in interactive context that hemoglobin exposure in state table matches final hemoglobin exposure pipeline value.

*** REMINDER ***
CI WILL NOT RUN ANY TESTS.
MANUALLY RUN TESTS WITH EACH PR.
-->

 all tests pass (pytest --runslow)

src/vivarium_gates_mncnh/data/loader.py
@@ -1065,17 +1065,12 @@ def load_coverage_from_file(filepath: Path, location: str) -> pd.DataFrame:
 def load_oral_iron_effect_size(
     key: str, location: str, years: Optional[Union[int, str, List[int]]] = None
 ) -> pd.DataFrame:
-    effect_size_dists = data_values.ORAL_IRON_EFFECT_SIZES[key]
-    effect_size_data = []
-
-    for target, dist in effect_size_dists.items():
-        draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
-        data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
-        data["affected_target"] = target
-        data = data.set_index("affected_target")
-        effect_size_data.append(data)
-
-    return pd.concat(effect_size_data)
+    dist = data_values.ORAL_IRON_EFFECT_SIZES[key]["hemoglobin.exposure"]
+    draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
+    data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
+    data["affected_target"] = "hemoglobin.exposure"
+    data = data.set_index("affected_target")
+    return data
 
 
 def load_ifa_excess_shift(
@@ -1107,9 +1102,10 @@ def load_risk_specific_shift(
     else:
         exposure = get_data(key_group.COVERAGE, location)
         excess_shift = get_data(key_group.EXCESS_SHIFT, location)
+        anc_proportion = get_data(data_keys.ANC.ANC1, location)
 
         risk_specific_shift = (
-            (exposure * excess_shift)
+            (exposure * excess_shift * anc_proportion)
             .groupby(
                 metadata.ARTIFACT_INDEX_COLUMNS + ["affected_entity", "affected_measure"]
             )

src/vivarium_gates_mncnh/components/hemoglobin.py
@@ -4,22 +4,35 @@ import numpy as np
 import pandas as pd
 import scipy
 from vivarium.framework.engine import Builder
+from vivarium.framework.event import Event
 from vivarium_public_health.risks.base_risk import Risk
 from vivarium_public_health.risks.distributions import MissingDataError
 from vivarium_public_health.risks.effect import NonLogLinearRiskEffect
 
 from vivarium_gates_mncnh.constants import data_keys
-from vivarium_gates_mncnh.constants.data_values import PIPELINES
+from vivarium_gates_mncnh.constants.data_values import PIPELINES, SIMULATION_EVENT_NAMES
 
 
 class Hemoglobin(Risk):
+    @property
+    def time_step_cleanup_priority(self) -> int:
+        # oral iron intervention will have been assigned
+        # when we update hemoglobin exposure in the state table
+        return 7
+
     def __init__(self) -> None:
         super().__init__("risk_factor.hemoglobin")
         self.exposure_pipeline_name = f"raw_{self.risk.name}.exposure"
 
     def setup(self, builder: Builder) -> None:
         super().setup(builder)
-        self.ifa_coverage = builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE).value[0]
+        self._sim_step_name = builder.time.simulation_event_name()
+        self.ifa_coverage = (
+            builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE)
+            .query("parameter=='cat2'")
+            .reset_index()
+            .value[0]
+        )
         self.ifa_effect_size = builder.data.load(
             data_keys.IFA_SUPPLEMENTATION.EFFECT_SIZE
         ).value[0]
@@ -29,14 +42,30 @@ class Hemoglobin(Risk):
             self.get_ifa_deleted_hemoglobin,
             component=self,
         )
-        builder.value.register_value_producer(
+        self.final_hemoglobin_exposure = builder.value.register_value_producer(
             PIPELINES.HEMOGLOBIN_EXPOSURE,
             source=lambda index: self.ifa_deleted_hemoglobin(index),
             component=self,
         )
 
+    def build_all_lookup_tables(self, builder: Builder) -> None:
+        self.lookup_tables["ANC1"] = self.build_lookup_table(
+            builder=builder,
+            data_source=builder.data.load(data_keys.ANC.ANC1),
+            value_columns=["value"],
+        )
+
     def get_ifa_deleted_hemoglobin(self, index: pd.Index) -> pd.Series[float]:
-        return self.exposure(index) - (self.ifa_effect_size * self.ifa_coverage)
+        return self.exposure(index) - (
+            self.ifa_effect_size * self.ifa_coverage * self.lookup_tables["ANC1"](index)
+        )
+
+    def on_time_step_cleanup(self, event: Event):
+        if self._sim_step_name() != SIMULATION_EVENT_NAMES.PREGNANCY:
+            return
+        exposure = self.final_hemoglobin_exposure(event.index)
+        exposure.name = self.exposure_column_name
+        self.population_view.update(exposure)
 
 
 class HemoglobinRiskEffect(NonLogLinearRiskEffect):

src/vivarium_gates_mncnh/components/intervention.py
@@ -222,7 +222,12 @@ class OralIronInterventionExposure(Component):
         self.randomness = builder.randomness.get_stream(self.name)
 
         self.scenario = builder.configuration.intervention.scenario
-        self.ifa_coverage = builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE).value[0]
+        self.ifa_coverage = (
+            builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE)
+            .query("parameter=='cat2'")
+            .reset_index()
+            .value[0]
+        )
 
         self.ifa_exposure_pipeline = builder.value.register_value_producer(
             self.ifa_exposure_pipeline_name,
@@ -303,7 +308,6 @@ class OralIronInterventionEffect(Component):
     #################
 
     def setup(self, builder: Builder) -> None:
-        self.ifa_coverage = builder.data.load(data_keys.IFA_SUPPLEMENTATION.COVERAGE).value[0]
         self.mms_stillbirth_rr = builder.data.load(
             data_keys.MMN_SUPPLEMENTATION.STILLBIRTH_RR
         ).value[0]

commit eb7b0ac2fc9aa444b7c014b7f23b876ee1fea36a

Model 19.0.1 artifact tweaks (#169)
* Fix bug with oral iron effect sizes function

This did not work for MMS, building an artifact from
scratch, because it hardcoded the target key.

This bug was introduced by https://github.com/ihmeuw/vivarium_gates_mncnh/commit/1fe36468dcb76358703be7bc04b63302e69c41ea#diff-c5d0e2dfb27ec86d7fb3005cf7080be104354f2bc93bca0724a0c510e4b0b304

I must admit, I do not really understand the purpose of that change.
Possibly a Chesterton's Fence blunder.

* Clean up issues with distributions

* Correct MMS to stillbirth RR

* Uncomment effect sizes, which were erroneously marked RT owned

* Check and better document data_values

src/vivarium_gates_mncnh/constants/data_keys.py
@@ -407,10 +407,9 @@ class __IFASupplementation(NamedTuple):
     COVERAGE: TargetString = TargetString(
         "risk_factor.iron_folic_acid_supplementation.coverage"
     )
-    # RT owned
-    # EFFECT_SIZE: TargetString = TargetString(
-    #     "risk_factor.iron_folic_acid_supplementation.effect_size"
-    # )
+    EFFECT_SIZE: TargetString = TargetString(
+        "risk_factor.iron_folic_acid_supplementation.effect_size"
+    )
     # EXCESS_SHIFT: TargetString = TargetString(
     #     "risk_factor.iron_folic_acid_supplementation.excess_shift"
     # )
@@ -434,10 +433,9 @@ IFA_SUPPLEMENTATION = __IFASupplementation()
 
 
 class __MMN_Supplementation(NamedTuple):
-    # RT owned
-    # EFFECT_SIZE: TargetString = TargetString(
-    #     "risk_factor.multiple_micronutrient_supplementation.effect_size"
-    # )
+    EFFECT_SIZE: TargetString = TargetString(
+        "risk_factor.multiple_micronutrient_supplementation.effect_size"
+    )
     STILLBIRTH_RR: TargetString = TargetString(
         "risk_factor.multiple_micronutrient_supplementation.stillbirth_rr"
     )

src/vivarium_gates_mncnh/constants/data_values.py
@@ -5,7 +5,6 @@ from vivarium_gates_mncnh.constants.data_keys import (
     FACILITY_CHOICE,
     IFA_SUPPLEMENTATION,
     MMN_SUPPLEMENTATION,
-    NO_ANTIBIOTICS_RISK,
     NO_AZITHROMYCIN_RISK,
     NO_CPAP_RISK,
 )
@@ -15,40 +14,6 @@ from vivarium_gates_mncnh.utilities import (
     get_truncnorm,
     get_uniform_distribution_from_limits,
 )
-
-############################
-# Disease Model Parameters #
-############################
-
-REMISSION_RATE = 0.1
-MEAN_SOJOURN_TIME = 10
-
-
-##############################
-# Screening Model Parameters #
-##############################
-
-PROBABILITY_ATTENDING_SCREENING_KEY = "probability_attending_screening"
-PROBABILITY_ATTENDING_SCREENING_START_MEAN = 0.25
-PROBABILITY_ATTENDING_SCREENING_START_STDDEV = 0.0025
-PROBABILITY_ATTENDING_SCREENING_END_MEAN = 0.5
-PROBABILITY_ATTENDING_SCREENING_END_STDDEV = 0.005
-
-FIRST_SCREENING_AGE = 21
-MID_SCREENING_AGE = 30
-LAST_SCREENING_AGE = 65
-
-
-###################################
-# Scale-up Intervention Constants #
-###################################
-SCALE_UP_START_DT = datetime(2021, 1, 1)
-SCALE_UP_END_DT = datetime(2030, 1, 1)
-SCREENING_SCALE_UP_GOAL_COVERAGE = 0.50
-SCREENING_SCALE_UP_DIFFERENCE = (
-    SCREENING_SCALE_UP_GOAL_COVERAGE - PROBABILITY_ATTENDING_SCREENING_START_MEAN
-)
-
 
 ##########################
 # Constant scalar values #
@@ -74,12 +39,6 @@ PREGNANCY_OUTCOMES = __PregnancyOutcome()
 
 
 class _Durations(NamedTuple):
-    FULL_TERM_DAYS = 40 * 7
-    POSTPARTUM_DAYS = 6 * 7
-    PARTURITION_DAYS = 1 * 7
-    DETECTION_DAYS = 6 * 7
-    PARTIAL_TERM_DAYS = 24 * 7
-    INTERVENTION_DELAY_DAYS = 8 * 7
     PARTIAL_TERM_LOWER_WEEKS = 6.0
     PARTIAL_TERM_UPPER_WEEKS = 24.0
 
@@ -141,10 +100,12 @@ HEMOGLOBIN_TEST_RESULTS = __HemoglobinTestResults()
 
 class __ANCRates(NamedTuple):
     RECEIVED_ULTRASOUND = {
+        # https://vivarium-research.readthedocs.io/en/latest/models/concept_models/vivarium_mncnh_portfolio/ai_ultrasound_module/module_document.html#id7
         "Ethiopia": 0.607,
         "Nigeria": 0.587,
         "Pakistan": 0.667,
     }
+    # https://vivarium-research.readthedocs.io/en/latest/models/concept_models/vivarium_mncnh_portfolio/ai_ultrasound_module/module_document.html#baseline-coverage
     ULTRASOUND_TYPE = {
         "Ethiopia": {
             ULTRASOUND_TYPES.STANDARD: 1.0,
@@ -164,6 +125,7 @@ class __ANCRates(NamedTuple):
         ULTRASOUND_TYPES.STANDARD: 0.61,
         ULTRASOUND_TYPES.AI_ASSISTED: 0.80,
     }
+    # https://vivarium-research.readthedocs.io/en/latest/models/concept_models/vivarium_mncnh_portfolio/ai_ultrasound_module/module_document.html#calculation-of-estimated-gestational-age
     STATED_GESTATIONAL_AGE_STANDARD_DEVIATION = {
         ULTRASOUND_TYPES.NO_ULTRASOUND: 10.0 / 7,
         ULTRASOUND_TYPES.STANDARD: 6.7 / 7,
@@ -297,7 +259,20 @@ class __Pipelines(NamedTuple):
 PIPELINES = __Pipelines()
 
 
+# https://vivarium-research.readthedocs.io/en/latest/models/causes/neonatal/preterm_birth.html#id5
 PRETERM_DEATHS_DUE_TO_RDS_PROBABILITY = 0.85
+# NOTE: This is an arbitrary value that must be greater than 0 and less than LATE_NEONATAL_AGE_START.
+# It is used in a tricky-to-understand way to deal with stillbirths.
+# Basically, to record YLLs due to stillbirths, we make stillbirth an "age group" for Vivarium purposes,
+# which spans from EARLY_NEONATAL_AGE_START (zero) to CHILD_INITIALIZATION_AGE.
+# Then when simulants are initialized, we put them in this stillbirth age group; if they are stillborn,
+# we have them die in that age group.
+# If they are a live birth, their age is updated to be greater than CHILD_INITIALIZATION_AGE so that they formally enter
+# the early neonatal age group.
+# It is a bit tricky to think through when we need to modify our approach to match this.
+# For example, we leave all early neonatal PAFs as starting at 0, which is fine because the early neonatal
+# period in GBD (0-7 days) is a superset of the early neonatal period in our sim (0.1 days-7 days) and
+# we don't apply any risk effects that would use a PAF to stillbirths.
 CHILD_INITIALIZATION_AGE = 0.1 / 365.0
 
 
@@ -331,60 +306,42 @@ DELIVERY_FACILITY_TYPE_PROBABILITIES = {
 # Probability each of these facility types has access to CPAP
 CPAP_ACCESS_PROBABILITIES = {
     "Ethiopia": {
+        # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/cpap_intervention.html#baseline-coverage-and-rr-data
         NO_CPAP_RISK.P_CPAP_BEMONC: get_norm(0.075, 0.02**2),
         NO_CPAP_RISK.P_CPAP_CEMONC: get_norm(0.393, 0.05**2),
         NO_CPAP_RISK.P_CPAP_HOME: get_norm(0.0, 0.00**2),
     },
+    # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/cpap_intervention.html#baseline-coverage-and-rr-data
     "Nigeria": {
         NO_CPAP_RISK.P_CPAP_BEMONC: get_norm(0.075, 0.02**2),
         NO_CPAP_RISK.P_CPAP_CEMONC: get_norm(0.393, 0.05**2),
         NO_CPAP_RISK.P_CPAP_HOME: get_norm(0.0, 0.00**2),
     },
+    # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/cpap_intervention.html#baseline-coverage-and-rr-data
     "Pakistan": {
         NO_CPAP_RISK.P_CPAP_BEMONC: get_norm(0.075, 0.02**2),
         NO_CPAP_RISK.P_CPAP_CEMONC: get_norm(0.393, 0.05**2),
         NO_CPAP_RISK.P_CPAP_HOME: get_norm(0.0, 0.00**2),
     },
 }
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/cpap_intervention.html#id7
 CPAP_RELATIVE_RISK_DISTRIBUTION = get_lognorm_from_quantiles(0.53, 0.34, 0.83)
 
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/intrapartum/acs_intervention.html#id22
 ACS_RELATIVE_RISK_DISTRIBUTION = get_lognorm_from_quantiles(0.84, 0.72, 0.97)
 
-
-ANTIBIOTIC_FACILITY_TYPE_DISTRIBUTION = {
-    # NOTE: This is not being used as of model 8.3
-    "Ethiopia": {
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_HOME: get_uniform_distribution_from_limits(0, 0.10),
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_BEMONC: get_uniform_distribution_from_limits(
-            0.302, 0.529
-        ),
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_CEMONC: get_uniform_distribution_from_limits(
-            0.768, 0.972
-        ),
-    },
-    "Nigeria": {
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_HOME: get_uniform_distribution_from_limits(0, 0.10),
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_BEMONC: get_uniform_distribution_from_limits(
-            0.302, 0.529
-        ),
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_CEMONC: get_uniform_distribution_from_limits(
-            0.768, 0.972
-        ),
-    },
-    "Pakistan": {
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_HOME: get_uniform_distribution_from_limits(0, 0.10),
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_BEMONC: get_uniform_distribution_from_limits(
-            0.302, 0.529
-        ),
-        NO_ANTIBIOTICS_RISK.P_ANTIBIOTIC_CEMONC: get_uniform_distribution_from_limits(
-            0.768, 0.972
-        ),
-    },
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/antibiotics_intervention.html#id32
+# Model 8.3+ sets coverage values at location level and not birth facility/location level
+ANTIBIOTIC_BASELINE_COVERAGE = {
+    "Ethiopia": 0.5,
+    "Nigeria": 0.0,
+    "Pakistan": 0.0,
 }
 ANTIBIOTIC_RELATIVE_RISK_DISTRIBUTION = get_lognorm_from_quantiles(0.78, 0.60, 1.00)
 
-
+# http://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/probiotics_intervention.html#baseline-coverage-data
 PROBIOTICS_BASELINE_COVERAGE_PROABILITY = 0.0
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/neonatal/probiotics_intervention.html#id14
 PROBIOTICS_RELATIVE_RISK_DISTRIBUTION = get_lognorm_from_quantiles(0.81, 0.72, 0.91)
 
 
@@ -410,6 +367,7 @@ INTERVENTION_TYPE_MAPPER = {
 
 
 AZITHROMYCIN_FACILITY_TYPE_DISTRIBUTION = {
+    # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/intrapartum/azithromycin_intervention.html#id12
     "Ethiopia": {
         NO_AZITHROMYCIN_RISK.P_AZITHROMYCIN_HOME: get_norm(0.0, 0.00**2),
         NO_AZITHROMYCIN_RISK.P_AZITHROMYCIN_BEMONC: get_norm(0.0, 0.00**2),
@@ -429,44 +387,57 @@ AZITHROMYCIN_FACILITY_TYPE_DISTRIBUTION = {
     },
 }
 # RR of no azithromycin intervention
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/intrapartum/azithromycin_intervention.html#id13
 AZITHROMYCIN_RELATIVE_RISK_DISTRIBUTION = get_lognorm_from_quantiles(1.54, 1.30, 1.82)
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/intrapartum/misoprostol_intervention.html#id17
 MISOPROSTOL_RELATIVE_RISK_DISTRIBUTION = get_lognorm_from_quantiles(0.61, 0.50, 0.74)
 
 # Effects of IV iron intervention
 IV_IRON_HEMOGLOBIN_EFFECT_SIZE = {
     # see research documentation here:  https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/mncnh_pregnancy/iv_iron_antenatal/iv_iron_mncnh.html#id16
-    "Ethiopia": get_norm(20.2, (21.5 - 18.9) / (2 * 1.96)),
-    "Nigeria": get_norm(20.2, (21.5 - 18.9) / (2 * 1.96)),
-    "Pakistan": get_norm(26.3, (26.9 - 25.7) / (2 * 1.96)),
+    "Ethiopia": get_norm(20.2, ninety_five_pct_confidence_interval=(18.9, 21.5)),
+    "Nigeria": get_norm(20.2, ninety_five_pct_confidence_interval=(18.9, 21.5)),
+    "Pakistan": get_norm(26.3, ninety_five_pct_confidence_interval=(25.7, 26.9)),
 }
 
 ORAL_IRON_EFFECT_SIZES = {
-    # IFA_SUPPLEMENTATION.EFFECT_SIZE: {
-    #     "hemoglobin.exposure": get_norm(
-    #         9.53, ninety_five_pct_confidence_interval=(6.99, 12.06)
-    #     ),
-    #     "birth_weight.birth_exposure": get_norm(
-    #         57.73, ninety_five_pct_confidence_interval=(7.66, 107.79)
-    #     ),
-    # },
-    # MMN_SUPPLEMENTATION.EFFECT_SIZE: {
-    #     "birth_weight.birth_exposure": get_norm(
-    #         45.16, ninety_five_pct_confidence_interval=(32.31, 58.02)
-    #     )
-    # },
+    IFA_SUPPLEMENTATION.EFFECT_SIZE: {
+        # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/mncnh_pregnancy/oral_iron_antenatal/oral_iron_antenatal.html#id24
+        "hemoglobin.exposure": get_norm(
+            9.53, ninety_five_pct_confidence_interval=(6.99, 12.06)
+        ),
+        # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/mncnh_pregnancy/oral_iron_antenatal/oral_iron_antenatal.html#id26
+        "birth_weight.birth_exposure": get_norm(
+            57.73, ninety_five_pct_confidence_interval=(7.66, 107.79)
+        ),
+    },
+    MMN_SUPPLEMENTATION.EFFECT_SIZE: {
+        # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/mncnh_pregnancy/oral_iron_antenatal/oral_iron_antenatal.html#id26
+        "birth_weight.birth_exposure": get_norm(
+            45.16, ninety_five_pct_confidence_interval=(32.31, 58.02)
+        )
+    },
+    # https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/mncnh_pregnancy/oral_iron_antenatal/oral_iron_antenatal.html#id31
     MMN_SUPPLEMENTATION.STILLBIRTH_RR: {
-        "stillbirth": get_lognorm_from_quantiles(0.53, 0.34, 0.83)
+        "stillbirth": get_lognorm_from_quantiles(0.91, 0.86, 0.98)
     },
 }
 
 
 # Postpartum depression constants
+# https://vivarium-research.readthedocs.io/en/latest/models/causes/maternal_disorders/gbd_2021_mncnh/postpartum_depression.html#id17
 POSTPARTUM_DEPRESSION_INCIDENCE_RISK = get_truncnorm(
-    0.12, ninety_five_pct_confidence_interval=(0.04, 0.20)
+    0.12,
+    ninety_five_pct_confidence_interval=(0.04, 0.20),
+    lower_clip=0.0,
+    upper_clip=1.0,
 )
 # Case duration is in years
+# https://vivarium-research.readthedocs.io/en/latest/models/causes/maternal_disorders/gbd_2021_mncnh/postpartum_depression.html#id17
 POSTPARTUM_DEPRESSION_CASE_DURATION = get_truncnorm(
-    0.65, ninety_five_pct_confidence_interval=(0.59, 0.70)
+    0.65,
+    ninety_five_pct_confidence_interval=(0.59, 0.70),
+    lower_clip=0.0,
 )
 
 
@@ -500,22 +471,38 @@ class __PostpartumDepressionCaseTypes(NamedTuple):
 POSTPARTUM_DEPRESSION_CASE_TYPES = __PostpartumDepressionCaseTypes()
 
 
+# https://vivarium-research.readthedocs.io/en/latest/models/causes/maternal_disorders/gbd_2021_mncnh/postpartum_depression.html#id18
 POSTPARTUM_DEPRESSION_CASE_SEVERITY_PROBABILITIES = {
     POSTPARTUM_DEPRESSION_CASE_TYPES.ASYMPTOMATIC: 0.14,
     POSTPARTUM_DEPRESSION_CASE_TYPES.MILD: 0.59,
     POSTPARTUM_DEPRESSION_CASE_TYPES.MODERATE: 0.17,
     POSTPARTUM_DEPRESSION_CASE_TYPES.SEVERE: 0.10,
 }
+# https://vivarium-research.readthedocs.io/en/latest/models/causes/maternal_disorders/gbd_2021_mncnh/postpartum_depression.html#id18
 POSTPARTUM_DEPRESSION_CASE_SEVERITY_DISABILITY_WEIGHTS = {
-    POSTPARTUM_DEPRESSION_CASE_TYPES.ASYMPTOMATIC: get_truncnorm(0.0, 0.00**2),
+    POSTPARTUM_DEPRESSION_CASE_TYPES.ASYMPTOMATIC: get_truncnorm(
+        0.0,
+        0.00**2,
+        lower_clip=0,
+        upper_clip=1,
+    ),
     POSTPARTUM_DEPRESSION_CASE_TYPES.MILD: get_truncnorm(
-        0.145, ninety_five_pct_confidence_interval=(0.099, 0.209)
+        0.145,
+        ninety_five_pct_confidence_interval=(0.099, 0.209),
+        lower_clip=0,
+        upper_clip=1,
     ),
     POSTPARTUM_DEPRESSION_CASE_TYPES.MODERATE: get_truncnorm(
-        0.396, ninety_five_pct_confidence_interval=(0.267, 0.531)
+        0.396,
+        ninety_five_pct_confidence_interval=(0.267, 0.531),
+        lower_clip=0,
+        upper_clip=1,
     ),
     POSTPARTUM_DEPRESSION_CASE_TYPES.SEVERE: get_truncnorm(
-        0.658, ninety_five_pct_confidence_interval=(0.477, 0.807)
+        0.658,
+        ninety_five_pct_confidence_interval=(0.477, 0.807),
+        lower_clip=0,
+        upper_clip=1,
     ),
 }
 
@@ -540,6 +527,7 @@ HEMOGLOBIN_ENSEMBLE_DISTRIBUTION_WEIGHTS = {
     ],
     "value": [0.4, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
 }
+# https://vivarium-research.readthedocs.io/en/latest/models/intervention_models/mncnh_pregnancy/anemia_screening.html#hemoglobin-screening-accuracy-instructions
 HEMOGLOBIN_TEST_SENSITIVITY = 0.85  # low hemoglobin that tests low
 HEMOGLOBIN_TEST_SPECIFICITY = 0.8  # adequate hemoglobin that tests adequate
 LOW_HEMOGLOBIN_THRESHOLD = 100

src/vivarium_gates_mncnh/data/loader.py
@@ -29,7 +29,7 @@ from vivarium_inputs import utility_data
 
 from vivarium_gates_mncnh.constants import data_keys, data_values, metadata, paths
 from vivarium_gates_mncnh.data import extra_gbd, sampling, utilities
-from vivarium_gates_mncnh.utilities import get_random_variable_draws
+from vivarium_gates_mncnh.utilities import get_random_variable_draws, get_truncnorm
 
 
 def get_data(
@@ -126,11 +126,10 @@ def get_data(
         data_keys.NO_MISOPROSTOL_RISK.RELATIVE_RISK: load_no_misoprostol_relative_risk,
         data_keys.NO_MISOPROSTOL_RISK.PAF: load_no_misoprostol_paf,
         data_keys.IFA_SUPPLEMENTATION.COVERAGE: load_ifa_coverage,
-        # RT owned
-        # data_keys.IFA_SUPPLEMENTATION.EFFECT_SIZE: load_oral_iron_effect_size,
+        data_keys.IFA_SUPPLEMENTATION.EFFECT_SIZE: load_oral_iron_effect_size,
         # data_keys.IFA_SUPPLEMENTATION.EXCESS_SHIFT: load_ifa_excess_shift,
         # data_keys.IFA_SUPPLEMENTATION.RISK_SPECIFIC_SHIFT: load_risk_specific_shift,
-        # data_keys.MMN_SUPPLEMENTATION.EFFECT_SIZE: load_oral_iron_effect_size,
+        data_keys.MMN_SUPPLEMENTATION.EFFECT_SIZE: load_oral_iron_effect_size,
         data_keys.MMN_SUPPLEMENTATION.STILLBIRTH_RR: load_oral_iron_effect_size,
         # RT owned
         # data_keys.MMN_SUPPLEMENTATION.EXCESS_SHIFT: load_mms_excess_shift,
@@ -267,8 +266,10 @@ def load_sbr(
     upper_value = sbr.loc[(year_start, year_end, "upper_value"), "value"]
     sbr = sbr.reorder_levels(["parameter", "year_start", "year_end"]).loc["mean_value"]
 
-    sbr_dist = sampling.get_truncnorm_from_quantiles(
-        mean=mean_value, lower=lower_value, upper=upper_value
+    sbr_dist = get_truncnorm(
+        mean=mean_value,
+        ninety_five_pct_confidence_interval=(lower_value, upper_value),
+        lower_clip=0.0,
     )
     sbr_draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, sbr_dist)
     sbr_draws = sbr_draws.values.flatten()
@@ -351,8 +352,11 @@ def load_anc_proportion(
         upper_value = anc_proportion.loc[(year_start, year_end, "upper_value"), "value"]
 
         try:
-            anc_proportion_dist = sampling.get_truncnorm_from_quantiles(
-                mean=mean_value, lower=lower_value, upper=upper_value
+            anc_proportion_dist = get_truncnorm(
+                mean=mean_value,
+                ninety_five_pct_confidence_interval=(lower_value, upper_value),
+                lower_clip=0.0,
+                upper_clip=1.0,
             )
             anc_proportion_draws = get_random_variable_draws(
                 metadata.ARTIFACT_COLUMNS, key, anc_proportion_dist
@@ -815,13 +819,8 @@ def load_antibiotic_coverage_probability(
     location: str,
     years: Optional[Union[int, str, List[int]]] = None,
 ) -> pd.DataFrame:
-    # Model 8.3 sets coverage values at population level and not birth facility/location level
-    coverage_dict = {
-        "Ethiopia": 0.5,
-        "Nigeria": 0.0,
-        "Pakistan": 0.0,
-    }
-    return coverage_dict[location]
+    # Model 8.3+ sets coverage values at location level and not birth facility/location level
+    return data_values.ANTIBIOTIC_BASELINE_COVERAGE[location]
 
 
 def load_no_antibiotics_relative_risk(
@@ -1108,12 +1107,17 @@ def load_coverage_from_file(filepath: Path, location: str) -> pd.DataFrame:
 def load_oral_iron_effect_size(
     key: str, location: str, years: Optional[Union[int, str, List[int]]] = None
 ) -> pd.DataFrame:
-    dist = data_values.ORAL_IRON_EFFECT_SIZES[key]["hemoglobin.exposure"]
-    draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
-    data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
-    data["affected_target"] = "hemoglobin.exposure"
-    data = data.set_index("affected_target")
-    return data
+    effect_size_dists = data_values.ORAL_IRON_EFFECT_SIZES[key]
+    effect_size_data = []
+
+    for target, dist in effect_size_dists.items():
+        draws = get_random_variable_draws(metadata.ARTIFACT_COLUMNS, key, dist)
+        data = pd.DataFrame([draws], columns=metadata.ARTIFACT_COLUMNS)
+        data["affected_target"] = target
+        data = data.set_index("affected_target")
+        effect_size_data.append(data)
+
+    return pd.concat(effect_size_data)
 
 
 def load_ifa_excess_shift(

src/vivarium_gates_mncnh/data/sampling.py
@@ -38,22 +38,3 @@ def generate_vectorized_lognormal_draws(
     draws = pd.concat([lognorm_samples, use_means])
     draws = draws.sort_index().rename(columns=lambda d: f"draw_{d}")
     return draws
-
-
-def get_truncnorm_from_quantiles(
-    mean: float,
-    lower: float,
-    upper: float,
-    quantiles: Tuple[float, float] = (0.025, 0.975),
-    lower_clip: float = 0.0,
-    upper_clip: float = 1.0,
-) -> stats.truncnorm:
-    stdnorm_quantiles = stats.norm.ppf(quantiles)
-    sd = (upper - lower) / (stdnorm_quantiles[1] - stdnorm_quantiles[0])
-    try:
-        a = (lower_clip - mean) / sd
-        b = (upper_clip - mean) / sd
-        return stats.truncnorm(loc=mean, scale=sd, a=a, b=b)
-    except ZeroDivisionError:
-        # degenerate case: if upper == lower, then use the mean with sd==0
-        return stats.norm(loc=mean, scale=sd)

src/vivarium_gates_mncnh/utilities.py
@@ -102,12 +102,21 @@ def get_truncnorm(
     mean: float,
     sd: float = None,
     ninety_five_pct_confidence_interval: Tuple[float, float] = None,
-    lower_clip: float = 0.0,
-    upper_clip: float = 1.0,
+    lower_clip: float = None,
+    upper_clip: float = None,
 ) -> stats.norm:
+    if lower_clip is None and upper_clip is None:
+        raise ValueError("Must provide either a lower or upper clip for truncnorm.")
     sd = _get_standard_deviation(mean, sd, ninety_five_pct_confidence_interval)
-    a = (lower_clip - mean) / sd if sd else mean - 1e-03
-    b = (upper_clip - mean) / sd if sd else mean + 1e03
+
+    if sd == 0:
+        # degenerate case: use stats.norm for this point mass
+        return stats.norm(loc=mean, scale=sd)
+
+    # See note on https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.truncnorm.html
+    # Use inf to avoid truncation (on one side) altogether: https://stackoverflow.com/a/61673195/
+    a = (lower_clip - mean) / sd if lower_clip is not None else -np.inf
+    b = (upper_clip - mean) / sd if upper_clip is not None else np.inf
     return stats.truncnorm(loc=mean, scale=sd, a=a, b=b)
 
 