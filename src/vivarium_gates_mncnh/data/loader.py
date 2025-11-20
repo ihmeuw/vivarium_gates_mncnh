7b383a19eb5c4d09e797d36818558fb694f924d9
Patrick Nast <pnast@uw.edu>
2025-11-20 14:59:06 -0800

    add data for abortion and ectopic pregnancy

--- src/vivarium_gates_mncnh/data/loader.py
+++ src/vivarium_gates_mncnh/data/loader.py
@@ -81,6 +81,12 @@ def get_data(
         data_keys.MATERNAL_HEMORRHAGE.RAW_INCIDENCE_RATE: load_standard_data,
         data_keys.MATERNAL_HEMORRHAGE.CSMR: load_standard_data,
         data_keys.MATERNAL_HEMORRHAGE.YLD_RATE: load_maternal_disorder_yld_rate,
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY.RAW_INCIDENCE_RATE: load_abortion_miscarriage_ectopic_incidence,
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY.CSMR: load_abortion_miscarriage_ectopic_csmr,
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY.YLD_RATE: load_abortion_miscarriage_ectopic_yld_rate,
+        # data_keys.RESIDUAL_MATERNAL_DISORDERS.RAW_INCIDENCE_RATE: load_residual_maternal_disorders_incidence,
+        # data_keys.RESIDUAL_MATERNAL_DISORDERS.CSMR: load_residual_maternal_disorders_csmr,
+        # data_keys.RESIDUAL_MATERNAL_DISORDERS.YLD_RATE: load_residual_maternal_disorders_yld_rate,
         data_keys.OBSTRUCTED_LABOR.RAW_INCIDENCE_RATE: load_standard_data,
         data_keys.OBSTRUCTED_LABOR.CSMR: load_standard_data,
         data_keys.OBSTRUCTED_LABOR.YLD_RATE: load_maternal_disorder_yld_rate,
@@ -383,6 +389,44 @@ def load_maternal_disorder_yld_rate(
     yld_rate = reshape_to_vivarium_format(yld_rate, location)
 
     return yld_rate
+
+
+def load_abortion_miscarriage_ectopic_incidence(
+    key: str, location: str, years: Optional[Union[int, str, list[int]]] = None
+) -> pd.DataFrame:
+    incidence_c374 = get_data(data_keys.PREGNANCY.RAW_INCIDENCE_RATE_ECTOPIC, location, years)
+    incidence_c995 = get_data(
+        data_keys.PREGNANCY.RAW_INCIDENCE_RATE_MISCARRIAGE, location, years
+    )
+    return incidence_c374 + incidence_c995
+
+
+def load_abortion_miscarriage_ectopic_csmr(
+    key: str, location: str, years: Optional[Union[int, str, list[int]]] = None
+) -> pd.DataFrame:
+    csmr_c374 = load_standard_data(
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY.ABORTION_MISCARRIAGE_CSMR,
+        location,
+        years,
+    )
+    csmr_c995 = load_standard_data(
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY, location, years
+    )
+    return csmr_c374 + csmr_c995
+
+
+def load_abortion_miscarriage_ectopic_yld_rate(
+    key: str, location: str, years: Optional[Union[int, str, list[int]]] = None
+) -> pd.DataFrame:
+    yld_rate_c374 = load_maternal_disorder_yld_rate(
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY.ABORTION_MISCARRIAGE_YLD_RATE,
+        location,
+        years,
+    )
+    yld_rate_c995 = load_maternal_disorder_yld_rate(
+        data_keys.ABORTION_MISCARRIAGE_ECTOPIC_PREGNANCY, location, years
+    )
+    return yld_rate_c374 + yld_rate_c995
 
 
 def load_lbwsg_rr(
