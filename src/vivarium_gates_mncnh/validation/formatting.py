83683524aaec3a6ff26334bf219b964dd970465b
Jim Albright <albrja@uw.edu>
2025-12-08 17:31:15 -0800

    Add implementation and tests for births formatter

---
+++ src/vivarium_gates_mncnh/validation/formatting.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+from vivarium_testing_utils.automated_validation.constants import DRAW_INDEX, SEED_INDEX
+from vivarium_testing_utils.automated_validation.data_transformation import calculations
+from vivarium_testing_utils.automated_validation.data_transformation.formatting import SimDataFormatter
+
+
+class TotalLiveBirths(SimDataFormatter):
+    """Formatter for simulation data that contains total live births."""
+
+    def __init__(self) -> None:
+        self.measure = "live_births"
+        self.entity = "total"
+        # Hardcoding raw dataset name due to custom observers
+        self.raw_dataset_name = "births"
+        self.unused_columns = [
+            "measure",
+            "entity_type",
+            "entity",
+            "sub_entity",
+        ]
+        self.filter_value = "total"
+        # NOTE: Sub entity column is currently null but will have useful values at some point
+        self.filters = {"sub_entity": [self.filter_value]}
+        self.name = f"{self.filter_value}_{self.measure}"
+
+    def format_dataset(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        """Clean up unused columns, and filter for the state."""
+        cols_to_marginalize = self.unused_columns + [DRAW_INDEX, SEED_INDEX]
+        return calculations.marginalize(dataset, cols_to_marginalize)
---
+++ tests/automated_v_and_v/test_formatting.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from vivarium_gates_mncnh.validation.formatting import TotalLiveBirths
+
+
+def test_births_formatter(get_births_observer_data: pd.DataFrame) -> None:
+    """Test Births formatter."""
+
+    formatter = TotalLiveBirths()
+
+    assert formatter.measure == "live_births"
+    assert formatter.raw_dataset_name == "births"
+    assert formatter.unused_columns == ["measure", "entity_type", "entity", "sub_entity"]
+    assert formatter.filters == {"sub_entity": ["total"]}
+
+    expected_dataframe = pd.DataFrame(
+        {
+            "value": [70.0, 150.0] * 2,
+        },
+        index=pd.MultiIndex.from_tuples(
+            [("Male", "A"), ("Male", "B"), ("Female", "A"), ("Female", "B")],
+            names=["child_sex", "common_stratify_column"],
+        ),
+    )
+
+    pd.testing.assert_frame_equal(formatter.format_dataset(get_births_observer_data), expected_dataframe)
+    
