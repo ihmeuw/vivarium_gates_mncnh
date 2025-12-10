e2fec49df60e76f55d743cbd7ca6ec18a20b2975
Jim Albright <albrja@uw.edu>
2025-12-10 10:54:18 -0800

    Address pr comments

--- src/vivarium_gates_mncnh/validation/formatting.py
+++ src/vivarium_gates_mncnh/validation/formatting.py
@@ -2,29 +2,22 @@ import pandas as pd
 from vivarium_testing_utils.automated_validation.constants import DRAW_INDEX, SEED_INDEX
 from vivarium_testing_utils.automated_validation.data_transformation import calculations
 from vivarium_testing_utils.automated_validation.data_transformation.formatting import (
-    TotalPopulationPersonTime,
+    SimDataFormatter,
 )
 
 
-class TotalLiveBirths(TotalPopulationPersonTime):
+class LiveBirths(SimDataFormatter):
     """Formatter for simulation data that contains total live births."""
 
     def __init__(self, scenario_columns: list[str]) -> None:
-        self.measure = "live_births"
-        self.entity = "total"
+        super().__init__(measure="live_births", entity="births", filter_value="live_birth")
         self.raw_dataset_name = "births"
-        self.name = "total_live_births"
-        self.filter_value = "total"
-        self.filters = {"sub_entity": [self.filter_value]}
-        self.scenario_columns = ["child_sex"] + scenario_columns
+        self.name = "live_births"
+        self.filters = {"pregnancy_outcome": [self.filter_value]}
+        self.scenario_columns = scenario_columns
         self.unused_columns = [
             "measure",
             "entity_type",
             "entity",
             "sub_entity",
         ]
-
-    def format_dataset(self, dataset: pd.DataFrame) -> pd.DataFrame:
-        # Filter to only live births
-        dataset = calculations.filter_data(dataset, {"pregnancy_outcome": ["live_birth"]})
-        return super().format_dataset(dataset)
--- tests/automated_v_and_v/test_formatting.py
+++ tests/automated_v_and_v/test_formatting.py
@@ -1,35 +1,60 @@
 import pandas as pd
 from vivarium_testing_utils.automated_validation.constants import DRAW_INDEX, SEED_INDEX
 
-from vivarium_gates_mncnh.validation.formatting import TotalLiveBirths
+from vivarium_gates_mncnh.validation.formatting import LiveBirths
 
 
 def test_births_formatter(get_births_observer_data: pd.DataFrame) -> None:
     """Test Births formatter."""
 
-    formatter = TotalLiveBirths([])
+    formatter = LiveBirths([])
 
     assert formatter.measure == "live_births"
     assert formatter.raw_dataset_name == "births"
     assert formatter.unused_columns == ["measure", "entity_type", "entity", "sub_entity"]
-    assert formatter.filters == {"sub_entity": ["total"]}
+    assert formatter.filters == {"pregnancy_outcome": ["live_birth"]}
 
     expected_dataframe = pd.DataFrame(
         {
-            "value": [40.0, 50.0, 60.0, 70.0, 21.0, 27.0, 40.0, 50.0],
+            "value": [
+                10.0,
+                15.0,
+                20.0,
+                25.0,
+                30.0,
+                35.0,
+                40.0,
+                45.0,
+                1.0,
+                2.0,
+                10.0,
+                15.0,
+                20.0,
+                25.0,
+                30.0,
+                35.0,
+            ],
         },
         index=pd.MultiIndex.from_tuples(
             [
-                ("Male", 0, 0),
-                ("Male", 0, 1),
-                ("Male", 1, 0),
-                ("Male", 1, 1),
-                ("Female", 0, 0),
-                ("Female", 0, 1),
-                ("Female", 1, 0),
-                ("Female", 1, 1),
+                ("Male", "A", 0, 0),
+                ("Male", "A", 0, 1),
+                ("Male", "A", 1, 0),
+                ("Male", "A", 1, 1),
+                ("Male", "B", 0, 0),
+                ("Male", "B", 0, 1),
+                ("Male", "B", 1, 0),
+                ("Male", "B", 1, 1),
+                ("Female", "A", 0, 0),
+                ("Female", "A", 0, 1),
+                ("Female", "A", 1, 0),
+                ("Female", "A", 1, 1),
+                ("Female", "B", 0, 0),
+                ("Female", "B", 0, 1),
+                ("Female", "B", 1, 0),
+                ("Female", "B", 1, 1),
             ],
-            names=["child_sex", DRAW_INDEX, SEED_INDEX],
+            names=["child_sex", "common_stratify_column", DRAW_INDEX, SEED_INDEX],
         ),
     )
 
