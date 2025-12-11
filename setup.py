32a99e6dadd77730a8bc4fcb16ab4a753571b8b6
Jim Albright <37345113+albrja@users.noreply.github.com>
2025-12-11 15:49:35 -0800

    Albrja/mic 6666/births formatter (#211)
    Albrja/mic 6666/births formatter
    
    Albrja/mic 6666/births formatter
    - *Category*: Feature/test
    - *JIRA issue*: https://jira.ihme.washington.edu/browse/MIC-6666
    - *Research reference*: <!--Link to research documentation for code -->
    
    Changes and notes
    -add births formatter to format births observer output

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     cluster_requirements = ["vivarium_cluster_tools>=2.0.0"]
     test_requirements = [
         "vivarium_dependencies[pytest]",
-        "vivarium_testing_utils",
+        "vivarium_testing_utils[validation] @ git+https://github.com/ihmeuw/vivarium_testing_utils.git@epic/mncnh-vv",
     ]
     lint_requirements = [
         "vivarium_dependencies[lint]",
