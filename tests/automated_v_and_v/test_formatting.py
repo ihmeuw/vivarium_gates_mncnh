d0a03f92cc48f721edd2cc12da456c67419fe7ad
Jim Albright <37345113+albrja@users.noreply.github.com>
2025-12-11 15:49:35 -0800

    Albrja/mic 6666/births formatter (#211)
    Albrja/mic 6666/births formatter
    
    Albrja/mic 6666/births formatter
    - *Category*: Feature/test
    - *JIRA issue*: https://jira.ihme.washington.edu/browse/MIC-6666
    - *Research reference*: <!--Link to research documentation for code -->
    
    Changes and notes
    -add births formatter to format births observer output

---
+++ tests/automated_v_and_v/test_formatting.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from vivarium_testing_utils.automated_validation.constants import DRAW_INDEX, SEED_INDEX
+
+from vivarium_gates_mncnh.validation.formatting import LiveBirths
+
+
+def test_births_formatter(get_births_observer_data: pd.DataFrame) -> None:
+    """Test Births formatter."""
+
+    formatter = LiveBirths([])
+
+    assert formatter.measure == "live_births"
+    assert formatter.raw_dataset_name == "births"
+    assert formatter.unused_columns == ["measure", "entity_type", "entity", "sub_entity"]
+    assert formatter.filters == {"pregnancy_outcome": ["live_birth"]}
+
+    expected_dataframe = pd.DataFrame(
+        {
+            "value": [
+                10.0,
+                15.0,
+                20.0,
+                25.0,
+                30.0,
+                35.0,
+                40.0,
+                45.0,
+                1.0,
+                2.0,
+                10.0,
+                15.0,
+                20.0,
+                25.0,
+                30.0,
+                35.0,
+            ],
+        },
+        index=pd.MultiIndex.from_tuples(
+            [
+                ("Male", "A", 0, 0),
+                ("Male", "A", 0, 1),
+                ("Male", "A", 1, 0),
+                ("Male", "A", 1, 1),
+                ("Male", "B", 0, 0),
+                ("Male", "B", 0, 1),
+                ("Male", "B", 1, 0),
+                ("Male", "B", 1, 1),
+                ("Female", "A", 0, 0),
+                ("Female", "A", 0, 1),
+                ("Female", "A", 1, 0),
+                ("Female", "A", 1, 1),
+                ("Female", "B", 0, 0),
+                ("Female", "B", 0, 1),
+                ("Female", "B", 1, 0),
+                ("Female", "B", 1, 1),
+            ],
+            names=["child_sex", "common_stratify_column", DRAW_INDEX, SEED_INDEX],
+        ),
+    )
+
+    pd.testing.assert_frame_equal(
+        formatter.format_dataset(get_births_observer_data), expected_dataframe
+    )
