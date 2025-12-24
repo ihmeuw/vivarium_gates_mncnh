e50f0b7c3699df43898c154e63bcb62a38f1aa0a
Jim Albright <albrja@uw.edu>
2025-12-23 16:13:35 -0800

    Add necessary files and directories

---
+++ tests/model_notebooks/test_notebooks.py
@@ -0,0 +1,5 @@
+"""
+Script to test Jupyter notebooks in the model_notebooks directory. This script 
+will run each each notebook and record whether the notebook runs succesfully or not. 
+If the notebook as any cell that errors out, the test will fail.
+"""
