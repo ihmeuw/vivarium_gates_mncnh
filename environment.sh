9ea7bd08d86e1f6fe497829ce1ed6a2aa34adb45
Patrick Nast <pnast@uw.edu>
2026-01-27 15:31:08 -0800

    simplify venv logic

--- environment.sh
+++ environment.sh
@@ -80,58 +80,46 @@ fi
 
 if [[ $use_shared == 'yes' ]]; then
   # Shared environment (venv overlay)
-  # Always call make - it handles existence checks and displays appropriate messages
-  force_flag=""
-  if [[ $make_new == 'yes' ]]; then
-    force_flag="force=yes"
+  venv_path=".venv/$env_name"
+  
+  if [[ ! -d $venv_path ]] || [[ $make_new == 'yes' ]]; then
+    # Venv doesn't exist or user requested force rebuild
+    make build-shared-env type=$env_type force=yes
   fi
-  
-  make build-shared-env type=$env_type $force_flag
-  
-  # Activate the venv
   source .venv/$env_name/bin/activate
 
 else
   # Conda environment
-  # Check staleness and only call make if needed
-  force_flag=""
-  create_env="no"
   lfs_flag=""
-
   if [[ $install_git_lfs == 'yes' ]]; then
     lfs_flag="lfs=yes"
   fi
+
+  env_info=$(conda info --envs | grep $env_name | head -n 1)
   
-  if [[ $make_new == 'yes' ]]; then
-    # User explicitly requested force rebuild
-    force_flag="force=yes"
-    create_env="yes"
-  else
-    # Check if environment exists and if stale
-    env_info=$(conda info --envs | grep $env_name | head -n 1)
-    if [[ $env_info != '' ]]; then
-      # Environment exists, check staleness
+  if [[ $env_info != '' ]]; then
+    # Environment exists
+    if [[ $make_new == 'yes' ]]; then
+      # User requested force rebuild
+      make build-env type=$env_type name=$env_name force=yes $lfs_flag
+      conda activate $env_name
+    else
+      # Check staleness
       conda activate $env_name
       expiration_time=$(date -d "$days_until_stale days ago" +%s)
       creation_time="$(head -n1 $CONDA_PREFIX/conda-meta/history)"
       creation_time=$(echo $creation_time | sed -e 's/^==>\ //g' -e 's/\ <==//g')
       creation_time="$(date -d "$creation_time" +%s)"
       if [[ $creation_time < $expiration_time ]]; then
-        # Environment is stale
-        force_flag="force=yes"
-        create_env="yes"
+        # Environment is stale, rebuild
+        make build-env type=$env_type name=$env_name force=yes $lfs_flag
+        conda activate $env_name
       fi
-    else
-      # Environment doesn't exist
-      create_env="yes"
+      # Otherwise already activated above, nothing more to do
     fi
-  fi
-  
-  if [[ $create_env == 'yes' ]]; then
-    make build-env type=$env_type name=$env_name $force_flag $lfs_flag
-    conda activate $env_name
   else
-    # Environment is up to date, just activate
-    [[ "$CONDA_DEFAULT_ENV" != "$env_name" ]] && conda activate $env_name || true
+    # Environment doesn't exist, create it
+    make build-env type=$env_type name=$env_name $lfs_flag
+    conda activate $env_name
   fi
 fi
