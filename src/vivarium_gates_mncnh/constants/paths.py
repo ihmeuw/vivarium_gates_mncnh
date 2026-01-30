84cee790dfaad2f58245ac507862060896910cd3
Hussain Jafari <hjafari@uw.edu>
2026-01-30 14:30:07 -0800

    little bit of DRYing

--- src/vivarium_gates_mncnh/constants/paths.py
+++ src/vivarium_gates_mncnh/constants/paths.py
@@ -8,26 +8,22 @@ BASE_DIR = Path(vivarium_gates_mncnh.__file__).resolve().parent
 ARTIFACT_ROOT = Path(
     f"/mnt/team/simulation_science/pub/models/{metadata.PROJECT_NAME}/artifacts/"
 )
+REPO_DATA_DIR = Path(__file__).parent.parent / "data"
 CLUSTER_DATA_DIR = Path("/mnt/team/simulation_science/pub/models/vivarium_gates_mncnh/data")
 ANC_DATA_DIR = CLUSTER_DATA_DIR / "antenatal_care"
 HEMOGLOBIN_SCREENING_DATA_DIR = CLUSTER_DATA_DIR / "hemoglobin_screening"
 
-LBWSG_PAF_OUTPUTS_DIR = Path(__file__).parent.parent / "data" / "lbwsg_paf" / "outputs"
-CALCULATED_PAFS_DIR = LBWSG_PAF_OUTPUTS_DIR / "calculated_pafs"
-PRETERM_PREVALENCE_DIR = LBWSG_PAF_OUTPUTS_DIR / "preterm_prevalence"
+LBWSG_PAF_OUTPUTS_DIR = REPO_DATA_DIR / "lbwsg_paf" / "outputs"
+CALCULATED_PAFS_DIR = LBWSG_PAF_OUTPUTS_DIR / "paf_outputs"
+PRETERM_PREVALENCE_DIR = LBWSG_PAF_OUTPUTS_DIR / "preterm_prevalence_outputs"
 
-LBWSG_RR_CAPS_DIR = Path(__file__).parent.parent / "data" / "lbwsg_rr_caps" / "caps"
-STILLBIRTH_RATIO_DATA_DIR = Path(__file__).parent.parent / "data" / "stillbirth_ratio"
-ORAL_IRON_DATA_DIR = Path(__file__).parent.parent / "data" / "ifa_mms_gestation_shifts"
-HEMOGLOBIN_EFFECTS_DATA_DIR = Path(__file__).parent.parent / "data" / "hemoglobin_effects"
-FERRITIN_TESTING_COVERAGE_DATA_DIR = (
-    Path(__file__).parent.parent / "data" / "ferritin_testing_coverage"
-)
+LBWSG_RR_CAPS_DIR = REPO_DATA_DIR / "lbwsg_rr_caps" / "caps"
+STILLBIRTH_RATIO_DATA_DIR = REPO_DATA_DIR / "stillbirth_ratio"
+ORAL_IRON_DATA_DIR = REPO_DATA_DIR / "ifa_mms_gestation_shifts"
+HEMOGLOBIN_EFFECTS_DATA_DIR = REPO_DATA_DIR / "hemoglobin_effects"
+FERRITIN_TESTING_COVERAGE_DATA_DIR = REPO_DATA_DIR / "ferritin_testing_coverage"
 FACILITY_CHOICE_OPTIMIZATION_RESULTS_CSV = (
-    Path(__file__).parent.parent
-    / "data"
-    / "facility_choice"
-    / "facility_choice_optimization_results.csv"
+    REPO_DATA_DIR / "facility_choice" / "facility_choice_optimization_results.csv"
 )
 
 # Update for new model results directory after model changes and runs
