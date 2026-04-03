95015c9354be0b77844eecbea95500dad7adf5df
Zeb Burke-Conte <zmbc@uw.edu>
2026-04-03 14:15:42 -0700

    Lint

--- tests/model_notebooks/test_notebooks.py
+++ tests/model_notebooks/test_notebooks.py
@@ -12,8 +12,8 @@ from typing import Any, Dict, List
 import papermill as pm
 import pytest
 from loguru import logger
-
 from vivarium_testing_utils.pytest_plugin import IS_ON_SLURM
+
 from vivarium_gates_mncnh.constants.paths import MODEL_NOTEBOOKS_DIR, MODEL_RESULTS_DIR
 
 
